5e5e5ba7e793966fd5bd42dbe66d5032859aa6f9
DSungatulin <sungatulindaniyar@gmail.com>
2024-01-05 22:56:37 +0600

    end of step 4

--- Makefile
+++ Makefile
@@ -27,4 +27,4 @@ gendiff:
 		poetry run gendiff
 
 test-coverage:
-	poetry run pytest --cov-report=xml --cov=gen_diff tests/
+	poetry run pytest --cov-report=xml --cov=gendiff tests/
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,10 +6,3 @@ def test_generate_diff():
     file_differences = generate_diff(first_file, second_file)
     correct_result = open('tests/fixtures/correct_comparison.txt', 'r')
     assert file_differences == correct_result.read()
-
-def test_generate_diff_2():
-    first_file = 'tests/fixtures/file1.json'
-    second_file = 'tests/fixtures/file2_extended.json'
-    file_differences = generate_diff(first_file, second_file)
-    correct_result = open('tests/fixtures/extended_file_comparison.txt', 'r')
-    assert file_differences == correct_result.read()
