ac18f40e9f5cc0e994d06e069e89ed1315f6f558
DSungatulin <sungatulindaniyar@gmail.com>
2024-02-13 01:04:27 +0600

    supplement the tests

---
+++ tests/test_parser.py
@@ -0,0 +1,14 @@
+import pytest
+from gendiff.parser import parse_file, determine_file_format
+
+
+def test_determine_file_format():
+    file1 = determine_file_format('tests/fixtures/flat_files/file1.json')
+    file2 = determine_file_format('tests/fixtures/flat_files/file2.yml')
+    assert file1 == {'follow': False, 'host': 'hexlet.io', 'proxy': '123.234.53.22', 'timeout': 50}
+    assert file2 == {'host': 'hexlet.io', 'timeout': 20, 'verbose': True}
+
+def test_determine_file_format_invalid():
+    with pytest.raises(TypeError) as excinfo:
+        determine_file_format('tests/fixtures/comparison_results/correct_flat_comparison.txt')
+    assert 'Invalid file format!' in str(excinfo.value)
