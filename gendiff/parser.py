d2fd66c872c17d9a0ba2fd570d168e6dfd7e73f1
DSungatulin <sungatulindaniyar@gmail.com>
2024-03-06 00:54:29 +0600

    fix mentor's comments 3

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,23 +1,19 @@
 import json
 import yaml
+import os
 
 
 def get_file_format(filepath):
-    file = open(filepath)
-    if filepath.endswith('.json'):
-        return file, 'json'
-    elif filepath.endswith('.yml') or filepath.endswith('.yaml'):
-        return file, 'yaml'
-    else:
-        file.close()
-        raise TypeError("Invalid file format!")
+    _, file_format = os.path.splitext(filepath)
+    return file_format[1:]
 
 
 def read_file_data(filepath):
-    file, file_format = get_file_format(filepath)
-    if file_format == 'json':
-        return json.load(file)
-    elif file_format == 'yaml':
-        return yaml.safe_load(file)
-    else:
-        raise TypeError("Invalid file format!")
+    with open(filepath) as file:
+        file_format = get_file_format(filepath)
+        if file_format == 'json':
+            return json.load(file)
+        elif file_format == 'yaml' or 'yml':
+            return yaml.load(file, Loader=yaml.Loader)
+        else:
+            raise TypeError("Invalid file format!")
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,18 @@
-import pytest
 from gendiff.parser import read_file_data
+
+
+def test_read_json_file_data():
+    filepath = 'tests/fixtures/flat_files/file1.json'
+    expected_data = {'follow': False, 'host': 'hexlet.io', 'proxy': '123.234.53.22', 'timeout': 50}
+    actual_data = read_file_data(filepath)
+    assert actual_data == expected_data
+
+
+def test_read_yaml_file():
+        filepath = 'tests/fixtures/flat_files/file2.yml'
+        expected_data = {'host': 'hexlet.io', 'timeout': 20, 'verbose': True}
+        actual_data = read_file_data(filepath)
+        assert actual_data == expected_data
 
 
 def test_determine_file_format():
@@ -7,9 +20,3 @@ def test_determine_file_format():
     file2 = read_file_data('tests/fixtures/flat_files/file2.yml')
     assert file1 == {'follow': False, 'host': 'hexlet.io', 'proxy': '123.234.53.22', 'timeout': 50}
     assert file2 == {'host': 'hexlet.io', 'timeout': 20, 'verbose': True}
-
-
-def test_determine_file_format_invalid():
-    with pytest.raises(TypeError) as excinfo:
-        read_file_data('tests/fixtures/comparison_results/correct_flat_comparison.txt')
-    assert 'Invalid file format!' in str(excinfo.value)
